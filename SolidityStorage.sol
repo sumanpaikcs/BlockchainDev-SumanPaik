a01d2cbab368dc262729284d0d16a673e275936c
Suman <sumanpaikcs@gmail.com>
2021-11-07 21:43:30 +0530

    This is the SolisityStorage file

---
+++ SolidityStorage.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.0;
+
+contract SolidityStorage {
+    
+    // this will get in. to zero;
+    uint256 public mynumber;
+    
+    function store(uint256 _mynumber) public{
+        mynumber = _mynumber;
+    }
+     
+     function retrieve() public view returns(uint256){
+         return mynumber;
+     }
+     
+}
+
